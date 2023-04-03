66e44682e86a8b83cfa2f575d1a4ac13d47ccffa
Conor Kelley <cfk102@gmail.com>
2023-04-03 01:38:54 -0400

    built first pass of bin_to_letter function

--- src/js/main.js
+++ src/js/main.js
@@ -33,3 +33,47 @@ mobileMenu_btn.addEventListener("click", () => {
     symbol_mid.classList.add("add-symbols");
   }
 });
+
+// Animate Binary Numbers turning into my name
+
+const letter_c = document.querySelector(".letter-c");
+const letter_o1 = document.querySelector(".letter-o1");
+const letter_n = document.querySelector(".letter-n");
+const letter_o2 = document.querySelector(".letter-o2");
+const letter_r = document.querySelector(".letter-r");
+
+// const letters = {
+//   letter_c: "2#v6%T-9QaBcK*pL$nXc",
+//   letter_o1: "&s@j1F!8zNw7fE^0Ggko",
+//   letter_n: "h3xU6R#Yl@VbP2W)oZ$n",
+//   letter_o2: "KfJ@9a0c^#xLsTt1yI*mo",
+//   letter_r: "7#Np@C1%tL9bXx$Hk2Dr",
+// };
+
+const letters = [letter_c, letter_o1, letter_n, letter_o2, letter_r];
+const hashes = [
+  "1029384756102938475c",
+  "9876543210987654321o",
+  "3141592653589793238n",
+  "1029384756102938475o",
+  "9876543210987654321r",
+];
+const delay = [0, 200, 400, 600, 800];
+
+const bin_to_letter = function (letter, binary, delay) {
+  setTimeout(() => {
+    let count = 0;
+    const interval = setInterval(() => {
+      if (count === binary.length) {
+        clearInterval(interval);
+      } else {
+        letter.innerText = binary[count];
+        count++;
+      }
+    }, 55);
+  }, delay);
+};
+
+for (let i in letters) {
+  bin_to_letter(letters[i], hashes[i], delay[i]);
+}
